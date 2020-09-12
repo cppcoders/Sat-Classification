167669686f209bdddb444e5724df9b33807e7294
sudofix <mo7amed.ram@gmail.com>
2020-09-12 21:18:41 +0200

    none

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def predict():
         pim = base64.b64encode(buffered.getvalue())
         j = {"img": str(pim)}
         r = requests.post(url, json=json.dumps(j)).json()
-        pred[str(pim)] = r["pred"]
+        pred[str(pim)[2:-1]] = r["pred"]
     return render_template('model.html', message=pred)
 
 
